a7f9a769c9b39605e2ee4908826aa2ff3ebb011e
Ivan de Castro <ivandecastroevangelista@gmail.com>
2024-09-08 14:32:32 -0300

    Montando a aula

--- tipo-number.js
+++ tipo-number.js
@@ -1,6 +1,9 @@
 const notaPrimeiroBi = 8;
 const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5');
 
-const total = notaPrimeiroBi + notaSegundoBi;
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
 
-console.log (total);
+const nome = 'ivan';
+console.log(`Meu nome é ${nome} e minha média é ${total.toFixed(2)}`);
