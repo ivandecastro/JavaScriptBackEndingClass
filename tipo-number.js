3bf21aea9edd6b3c3c221f2b8c73dcd82bd8c798
Ivan de Castro <ivandecastroevangelista@gmail.com>
2024-09-08 14:34:13 -0300

    Corrigindo o texto

--- tipo-number.js
+++ tipo-number.js
@@ -6,4 +6,4 @@ const notaQuartoBi = Number.parseInt('5');
 const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi) / 4;
 
 const nome = 'ivan';
-console.log(`Meu nome é ${nome} e minha média é ${total.toFixed(2)}`);
+console.log(`Meu nome é ${nome} e minha média é de ${total.toFixed(2)}`);
