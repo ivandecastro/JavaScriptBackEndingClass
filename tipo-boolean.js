37ef491b517c27ce5831c84ea3951d792828c1da
Ivan de Castro <ivandecastroevangelista@gmail.com>
2024-09-08 15:06:43 -0300

    Terminando a aula sobre 'Tipos booleanos'.

---
+++ tipo-boolean.js
@@ -0,0 +1,6 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+estaAprovado ? console.log('parabens, boas festas') : console.log('REPROVADO, boas festas');
+
+estudante === 'Fernando' ? console.log(`olá, ${estudante}`) : console.log("Who're u?");
